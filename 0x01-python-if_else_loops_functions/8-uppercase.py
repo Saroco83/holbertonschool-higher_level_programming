ae5e4f6c88ea8d3b982cedc6134dd01d3270557c
Saroco83 <estudialondres@gmail.com>
2020-09-15 06:27:00 +0000

    end= + ord() + chr()

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def uppercase(str):
-    print("".join(["{}".format(chr(ord(i)-32)) if 96 < ord(i) < 122 else
+    print("".join(["{}".format(chr(ord(i)-32)) if 96 < ord(i) < 123 else
                    i for i in str]))
